d7c271c0d9d9353872f340f4ce70def0b9930b2f
Hugh Rawlinson <hughr2005@gmail.com>
2021-08-21 15:56:44 +0200

    the start of the website

--- __tests__/epsilon-greedy.ts
+++ __tests__/epsilon-greedy.ts
@@ -6,16 +6,20 @@ import { NamedArm, shuffle, testAlgorithm } from "../testing";
 describe("EpsilonGreedy", () => {
   it("should return a valid probability distribution", () => {
     const means = [0.1, 0.1, 0.1, 0.1, 0.9];
-    const arms: NamedArm[] = shuffle(means).map((mean, index) => ({
+    // const arms: NamedArm[] = shuffle(means).map((mean, index) => ({
+    const arms: NamedArm[] = means.map((mean, index) => ({
       __type: "arm" as const,
       name: "" + index,
-      arm: new BernoulliArm(mean)
+      arm: new BernoulliArm(mean),
     }));
 
     const epsilonGreedy = new EpsilonGreedy(0.1);
-    
-    const testResult = testAlgorithm(epsilonGreedy, arms, 100, 10);
 
-    writeFileSync("epsilon-greedy.json", JSON.stringify(testResult, null, 2));
+    const testResult = testAlgorithm(epsilonGreedy, arms, 3, 250);
+
+    writeFileSync(
+      "results/epsilon-greedy.json",
+      JSON.stringify(testResult, null, 2)
+    );
   });
 });
